be37c1eb0550eefc520c5993a76c5251e30817a8
JunQuann <limjunquann@gmail.com>
2020-06-13 22:20:28 +0800

    edit port for backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("Hello World");
+  res.send("Welcome to TechLink backend service");
 });
 
 module.exports = router;
